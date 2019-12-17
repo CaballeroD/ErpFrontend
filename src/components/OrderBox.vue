abf601c1c9e8a7cada2c33391db246e7d9b79f60
CaballeroD <manuelggwp111@gmail.com>
2019-12-17 18:44:58 +0000

    First version

---
+++ src/components/OrderBox.vue
@@ -0,0 +1,32 @@
+<template>
+  <v-row justify="center" align="alignment">
+    <v-expansion-panels
+      :accordion="accordion"
+      :popout="popout"
+      :inset="inset"
+      :multiple="multiple"
+      :focusable="focusable"
+      :disabled="disabled"
+      :readonly="readonly"
+    >
+      <v-expansion-panel v-for="(item,i) in 5" :key="i">
+        <v-expansion-panel-header>Item</v-expansion-panel-header>
+        <v-expansion-panel-content>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</v-expansion-panel-content>
+      </v-expansion-panel>
+    </v-expansion-panels>
+  </v-row>
+</template>
+<script>
+export default {
+  name: "orderbox",
+  data: () => ({
+    accordion: false,
+    popout: false,
+    inset: false,
+    multiple: false,
+    disabled: false,
+    readonly: false,
+    focusable: true
+  })
+};
+</script>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,17 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <DarkLayout/>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
+import DarkLayout from "@/components/DarkLayout.vue";
 
 export default {
   name: "home",
   components: {
-    HelloWorld
+    DarkLayout
   }
 };
 </script>
