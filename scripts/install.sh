2bdc6bd53d2f706bbe1d893fac4e655e4149fcce
CaballeroD <manuelggwp111@gmail.com>
2020-06-10 19:17:00 +0000

    prueba despliegue

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-openssl aes-256-cbc -K $encrypted_f217180e22ee_key -iv $encrypted_f217180e22ee_iv -in id_rsa.enc -out id_rsa -d
+openssl aes-256-cbc -K $encrypted_f217180e22ee_key -iv $encrypted_f217180e22ee_iv -in id_rsa.enc -out deploy-travis -d
 
 eval $(ssh-agent -s)
 
-chmod 600 id_rsa
+chmod 600 deploy-travis
 
-ssh-add id_rsa
+ssh-add deploy-travis
