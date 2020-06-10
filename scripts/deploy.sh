efb26f21940e4b9517dc4437eb586f7b2d2ed4c5
CaballeroD <manuelggwp111@gmail.com>
2020-06-10 20:20:53 +0000

    prueba despliegue

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
-scp -o StrictHostKeyChecking=no -i deploy-travis -v -r dist ${REMOTE_USER}@${REMOTE_HOST}:${REMOTE_PATH}
+#scp -o StrictHostKeyChecking=no -i deploy-travis -v -r dist ${REMOTE_USER}@${REMOTE_HOST}:${REMOTE_PATH}
+ssh -o StrictHostKeyChecking=no -i deploy-travis -vp ${REMOTE_USER}@${REMOTE_HOST} touch prueba.txt
